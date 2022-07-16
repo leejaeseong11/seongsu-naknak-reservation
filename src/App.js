78be67a921fcfde5e51f116e021a119df018fe3f
leejaeseong11 <easeon78@gmail.com>
2022-07-17 00:29:49 +0900

    feat: 캘린더 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import ReservationDate from './components/ReservationDate';
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <ReservationDate />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ReservationDate.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import moment from 'moment';
+import './ReservationDate.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // css import
+
+function ReservationDate() {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+      <div className="text-gray-500 mt-4">
+        {moment(value).format('YYYY년 MM월 DD일')}
+      </div>
+    </div>
+  );
+}
+
+export default ReservationDate;
